7ddc0a38d19d81262a16ce2359f34ce0cd20522b
Justin Kim <justin.kim@collabora.com>
2017-10-26 14:08:57 +0900

    m

--- haicrypt/hcrypt-gnutls.c
+++ haicrypt/hcrypt-gnutls.c
@@ -16,24 +16,31 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; If not, see <http://www.gnu.org/licenses/>
  */
-#ifndef __HCRYPT_GNUTLS_H__
-#define __HCRYPT_GNUTLS_H__
+#include "hcrypt-gnutls.h"
 
-#include <gnutls/gnutls.h>
-#include <gnutls/crypto.h>
+#if     __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ > 4)
+#define GNUC_UNUSED                           \
+  __attribute__((__unused__))
+#else
+#define GNUC_UNUSED
+#endif
 
-#include <nettle/aes.h>
-#include <nettle/pbkdf2.h>
+int
+hcrypt_aes_set_encrypt_key (const uint8_t *key,
+                            unsigned bits GNUC_UNUSED,
+                            struct aes128_ctx *ctx)
+{
+  /* Assuming AES128 encryption, ignore the number of bits */
+  aes128_set_encrypt_key (ctx, key);
+  return 0;
+}
 
-typedef struct aes128_ctx AES_KEY;
-
-#define hcrypt_Prng(rn,len) (gnutls_rnd(GNUTLS_RND_KEY,(rn),(len)) <= 0 ? -1 : 0)
-
-#define hcrypt_pbkdf2_hmac_sha1(p,p_len,sa,sa_len,itr,out_len,out) \
-   pbkdf2_hmac_sha1(p_len,(const uint8_t *)p,itr,sa_len,sa,out_len,out)
-
-int hcrypt_aes_set_encrypt_key (const uint8_t *key, unsigned bits, struct aes128_ctx *ctx);
-int hcrypt_aes_set_decrypt_key (const uint8_t *key, unsigned bits, struct aes128_ctx *ctx);
-
-#endif /* __HCRYPT_GNUTLS_H__ */
-
+int
+hcrypt_aes_set_decrypt_key (const uint8_t *key,
+                            unsigned bits GNUC_UNUSED,
+                            struct aes128_ctx *ctx)
+{
+  /* Assuming AES128 decryption, ignore the number of bits */
+  aes128_set_decrypt_key (ctx, key);
+  return 0;
+}
